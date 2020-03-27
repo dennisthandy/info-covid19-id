bd08c3ad192c22549744a9bb10af80aa1ea5567a
Dennis Thandy Nur Cahyono <33387667+dennisthandy@users.noreply.github.com>
2020-03-28 05:50:48 +0700

    Chages API to NovelCOVID API

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ const confirmed = document.querySelector('#confirmed');
 const deaths = document.querySelector('#deaths');
 const recovered = document.querySelector('#recovered');
 
-fetch("https://pomber.github.io/covid19/timeseries.json")
+fetch("https://corona.lmao.ninja/countries/Indonesia")
   .then(response => response.json())
   .then(data => {
-    let indonesia = data["Indonesia"];
-    console.log(indonesia)
-    let update = indonesia[indonesia.length - 1];
-    to.innerHTML = formatDate(update.date);
-    confirmed.innerHTML = update.confirmed;
-    deaths.innerHTML = update.deaths;
-    recovered.innerHTML = update.recovered;
+    console.log(data);
+    to.innerHTML = formatDate();
+    confirmed.innerHTML = data.cases;
+    deaths.innerHTML = data.deaths;
+    recovered.innerHTML = data.recovered ;
 });
 
-function formatDate(date) {
-  let year = date.slice(0,4);
-  let month = (date.slice(5,6) < 10) ? ('0' + date.slice(5,6)) : (date.slice(5,6));
-  let day = (date.slice(7) < 10) ? ('0' + date.slice(7)) : (date.slice(7));
-  return day + '/' + month +'/' +year;
+function formatDate() {
+  var today = new Date();
+  var dd = String(today.getDate() - 1).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+
+  today = dd + '/' + mm + '/' + yyyy;
+  return today;
 }
