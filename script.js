00c2c4342b3ac0f4dcd87a2b1ab83552fd200073
Dennis Thandy Nur Cahyono <33387667+dennisthandy@users.noreply.github.com>
2020-04-05 20:27:16 +0700

    update theme

--- script.js
+++ script.js
@@ -17,15 +17,38 @@ fetch("https://corona.lmao.ninja/countries/Indonesia")
       deaths.innerHTML = data.deaths;
       recovered.innerHTML = data.recovered;
     }
-
 });
 
 function formatDate() {
   var today = new Date();
-  var dd = String(today.getDate()).padStart(2, '0');
+  var dd = String(today.getDate() - 1).padStart(2, '0');
   var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
   var yyyy = today.getFullYear();
 
   today = dd + '/' + mm + '/' + yyyy;
   return today;
+}
+
+//social code
+const socialOpen = document.querySelector('.open');
+const socialClose = document.querySelector('.close');
+const socialContainer = document.querySelector('.social-container');
+
+socialOpen.addEventListener('click', showSocial);
+socialClose.addEventListener('click', closeSocial);
+
+function showSocial() {
+  socialOpen.style.display = 'none';
+  socialClose.style.display = 'inline';
+  socialContainer.style.opacity = '1';
+  socialContainer.style.transform = 'scale(1)';
+  socialContainer.style.right = '100%';
+}
+
+function closeSocial() {
+  socialOpen.style.display = 'inline';
+  socialClose.style.display = 'none';
+  socialContainer.style.opacity = '0';
+  socialContainer.style.transform = 'scale(.5)';
+  socialContainer.style.right = '-250%';
 }
