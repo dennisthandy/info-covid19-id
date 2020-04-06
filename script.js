5289488924fac7a2f446da308bf232acd5d15bf2
Dennis Thandy Nur Cahyono <33387667+dennisthandy@users.noreply.github.com>
2020-04-06 16:55:10 +0700

    fix date

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ fetch("https://corona.lmao.ninja/countries/Indonesia")
 
 function formatDate() {
   var today = new Date();
-  var dd = String(today.getDate() - 1).padStart(2, '0');
+  var dd = String(today.getDate()).padStart(2, '0');
   var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
   var yyyy = today.getFullYear();
 
